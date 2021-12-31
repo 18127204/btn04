1687c4631f5b4813d49a4ed2885494b558c22d14
18127024 <quangminhtran323@gmail.com>
2021-12-31 15:41:13 +0700

    mark final decision be

--- backend/routes/GradeReview/index.js
+++ backend/routes/GradeReview/index.js
@@ -74,4 +74,135 @@ router.get('/api/getAllComments/:link/:idassiment', function (req, res, next) {
     }
     )(req, res, next);
 });
+router.post('/api/addComment', function (req, res, next) {
+    passport.authenticate("jwt", { session: false, }, function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            res.header({ "Access-Control-Allow-Origin": "*" });
+            res.status(401);
+            res.send({ message: info.message, success: false });
+            return;
+        }
+        let {comment,parentid,createat,assignmentId,finalgrade}=req.body;
+        const sql = `INSERT INTO comment (comment,username,accountId,parentid,createat,assignmentId,finalgrade)
+        VALUES (?,?,?,?,?,?,?);
+        `
+        pool.query(sql, [comment,user.username,user.id,parentid,createat,assignmentId,finalgrade], (error, result) => {
+            if (error) {
+                res.send(error);
+            }
+            else {
+                res.json(result);
+            }
+        });
+    }
+    )(req, res, next);
+});
+
+/* /api/addComment
+ dataSend:{comment:'abcd',parentId:'12',createat:'2021-08-17T23:00:33.010+02:00',assignmentId:'333',finalgrade:'-1'}
+/gradeReview/api/addFinalDecision
+*/
+
+
+router.post('/api/addFinalDecision', function (req, res, next) {
+    passport.authenticate("jwt", { session: false, }, function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            res.header({ "Access-Control-Allow-Origin": "*" });
+            res.status(401);
+            res.send({ message: info.message, success: false });
+            return;
+        }
+        let {link,comment,parentid,createat,assignmentId,finalgrade}=req.body;
+        const sqlrole = `SELECT CA.role FROM classes C INNER JOIN classaccount CA ON C.id=CA.classId INNER JOIN account A
+        ON A.id=CA.accountId  WHERE C.link=? and CA.accountId=? 
+        `;
+        pool.query(sqlrole, [link,user.id], (error, result) => {
+            if (error) {
+                res.send(error);
+            }
+            else {
+                if (result[0].role ==="teacher") {
+                    const sqlinsert = `INSERT INTO comment (comment,username,accountId,parentid,createat,assignmentId,finalgrade)
+                    VALUES (?,?,?,?,?,?,?)
+                    `
+                    pool.query(sqlinsert, [comment,user.username,user.id,parentid,createat,assignmentId,finalgrade], (error, result1) => {
+                        if (error) {
+                            res.send(error);
+                        }
+                        else {
+                            const sqlIdCmt =`SELECT * FROM comment WHERE id=LAST_INSERT_ID()`;
+                            pool.query(sqlIdCmt, [], (error, result2) => {
+                                if (error) {
+                                    res.send(error);
+                                }
+                                else {
+                                    const sqlparent = `SELECT * FROM comment WHERE id=?`;
+                                    pool.query(sqlparent, [result2[0].parentid], (error, result3) => {
+                                        if (error) {
+                                            res.send(error);
+                                        }
+                                        else {
+                                            if (result3[0].finalgrade ==-1) {
+                                                const updatefinal=`UPDATE comment
+                                                SET finalgrade =? WHERE id =? `
+                                                pool.query(updatefinal, [finalgrade,result3[0].id], (error, result4) => {
+                                                    if (error) {
+                                                        res.send(error);
+                                                    }
+                                                    else {
+                                                        const sqlmssv =`SELECT  DISTINCT G.mssv from
+                                                        comment C INNER JOIN grade G ON G.assignmentId=C.assignmentId
+                                                               INNER JOIN account A ON G.mssv=A.mssv
+                                                               WHERE A.id=?
+                                                        `
+                                                        pool.query(sqlmssv, [result3[0].accountId], (error, result5) => {
+                                                            if (error) {
+                                                                res.send(error);
+                                                            }
+                                                            else {
+        
+                                                                const updategrade= `UPDATE grade
+                                                                SET grade =? WHERE mssv=? and assignmentId=?`;
+                                                                pool.query(updategrade, [finalgrade,result5[0].mssv,assignmentId], (error, result6) => {
+                                                                    if (error) {
+                                                                        res.send(error);
+                                                                    }
+                                                                    else {
+                                                                        res.json(result6);
+                                                                        
+                                                                    }
+                                                                });
+
+                                                            }
+                                                        });
+                                                    }
+                                                });
+
+                                            }
+                                            else {
+                                                res.json({message:"grade decision"});
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                          
+                        }
+                    });
+                }
+                else if (result[0].role ==="student"){
+                    res.json({message:"you don't have right mark descision"});
+                }
+            }
+        });
+    }
+    )(req, res, next);
+});
+
 module.exports = router;
